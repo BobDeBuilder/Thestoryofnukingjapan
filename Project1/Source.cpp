5cdcaac03981284b9a2cebb3634f8ac0759ec987
agrecascino <agrecascino123@gmail.com>
2015-06-09 23:15:56 -0400

    Initial commit
    FUCK BACCA

---
+++ Project1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool bJapanIsNotNuked = true;
+
+int main()
+{
+	cout << "have not setup japan nukes" << endl;
+	while (bJapanIsNotNuked)
+	{
+		cout << "todo" << endl;
+	}
+	return 0;
+}
