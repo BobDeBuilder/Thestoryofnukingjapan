dd15faba2daffe52dcf44585cf65cdebc2827da0
redcrew3 <redcrew1995@gmail.com>
2015-06-09 22:05:45 -0700

    Update PLAYER.h

--- Project1/PLAYER.h
+++ Project1/PLAYER.h
@@ -1,14 +1,18 @@
 //player class
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
 
 class Player{
 private:
-    int choice;  //use this like if there is a menu and you need to choose a number to select an option
+    int choice; //use this like if there is a menu and you need to choose a number to select an option
+    std::string; 
 public:
     Player();
     ~Player();
     void setChoice(int choice);
+    void setName(std::string name);
+    void displayName();
     int getChoice();
 };
 
