4201ea22b17250669b6f41ada3ec8a7bb1ca668a
redcrew3 <redcrew1995@gmail.com>
2015-06-09 22:31:08 -0700

    Update Nclock.cpp
    added function to include a clock that starts at 7 AM and you can increment the time until it hits the desired time declared in the main (8:16)

--- Project1/Nclock.cpp
+++ Project1/Nclock.cpp
@@ -1,5 +1,21 @@
-  void add1Min();
-  void add5Min();
-  void add10Min();
-  void add30Min();
-  int getTime();
+  #include "NCLOCK.h"
+  
+  Clock::Clock(){
+    t_ime = 7.00
+  }
+  Clock::~Clock(){}
+  void Clock::add1Min(){
+    t_ime += 0.01;
+  }
+  void Clock::add5Min(){
+    t_ime += 0.05;
+  }
+  void Clock::add10Min(){
+    t_ime += 0.10;
+  }
+  void Clock::add30Min(){
+    t_ime += 0.30;
+  }
+  int Clock::getTime(){
+    return t_ime;
+  }
