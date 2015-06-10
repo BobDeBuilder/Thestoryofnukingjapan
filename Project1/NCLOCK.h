f05051a6f964210b4880f4b4532f1003e6bc5fdf
redcrew3 <redcrew1995@gmail.com>
2015-06-10 08:11:55 -0700

    Update NCLOCK.h

--- Project1/NCLOCK.h
+++ Project1/NCLOCK.h
@@ -6,11 +6,13 @@ private:
   float t_ime;
 public:
   Clock();
+  Clock(float t_ime);
   ~Clock();
   void add1Min();
   void add5Min();
   void add10Min();
   void add30Min();
+  void setTime();
   int getTime();
 };
 
